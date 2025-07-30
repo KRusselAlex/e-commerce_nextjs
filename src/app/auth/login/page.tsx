608e44e0bb3ac77ab1e9e70a5672fd30e5d8ad97
KAlexRussel <kouawoualex1234@gmail.com>
2025-07-30 16:19:09 +0100

    [REF]: update the design of pages puting spaces

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,16 +3,16 @@ import Logo from "@/components/logo/logo";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen  flex-col bg-muted">
-      <div className="flex flex-col max-w-7xl mx-auto w-full">
+    <div className="bg-muted h-full">
+      <div className="flex min-h-screen flex-col justify-center  relative h-full max-w-7xl mx-auto w-full">
         {/* Top nav / logo */}
-        <header className="flex h-16 items-center justify-center border-b px-4 md:justify-start md:px-8">
+        <header className=" flex justify-center mt-3 md:mt-0 md:absolute md:top-4 md:left-0  z-10">
           <Logo />
         </header>
 
         {/* Main content */}
-        <main className="flex flex-1 items-center justify-center p-4 md:p-6 lg:p-8">
-          <div className="w-full max-w-3xl">
+        <main className="flex flex-1 items-center  justify-center p-4 h-full md:p-6 lg:p-8">
+          <div className="w-full max-w-4xl h-full">
             <LoginForm />
           </div>
         </main>
--- src/components/LoginForm/loginForm.tsx
+++ src/components/LoginForm/loginForm.tsx
@@ -85,10 +85,16 @@ export function LoginForm({
   };
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="overflow-hidden">
-        <CardContent className="grid p-0 md:grid-cols-2">
-          <form onSubmit={handleSubmit(onSubmit)} className="p-6 md:p-8">
+    <div
+      className={cn("flex flex-col gap-6 first-line h-full ", className)}
+      {...props}
+    >
+      <Card className="overflow-hidden h-full">
+        <CardContent className="grid p-0 md:grid-cols-2 h-full">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="p-6 md:p-16 h-full"
+          >
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Bienvenue</h1>
@@ -167,7 +173,7 @@ export function LoginForm({
               src="/equipements.jpeg"
               alt="Image"
               fill
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              className="absolute inset-0 min-h-4xl h-full  w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
           </div>
         </CardContent>
